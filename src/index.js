ba4b220e24a3eb3694426a43f51f5659dcb43eec
adela <adelabbok@gmail.com>
2020-07-09 12:27:03 +0900

    Bare Minimum Requirements 2_Event

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const GroceryList  = (props) => {
+  const listItemClick = (e) => {
+    alert('clicked!');
+  }
+  return (<ul>
+    {props.groceries.map(item => <li onClick={listItemClick}>{item}</li>)}
+  </ul>)
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => (
+  <div>
+    <h1>My Grocery List</h1>
+    <GroceryList
+      groceries={[
+        'Kale', 'Cucumbers', 'Cabage'
+      ]} />
+  </div>
+)
+
+ReactDOM.render(<App />, document.getElementById("root"));
