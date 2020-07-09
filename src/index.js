8b658ba9445542222de9a4f104b9b3ff654a02df
adela <adelabbok@gmail.com>
2020-07-09 12:31:05 +0900

    Bare Minimum Requirements 3_hover event추가

--- src/index.js
+++ src/index.js
@@ -2,12 +2,58 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const GroceryList  = (props) => {
-  const listItemClick = (e) => {
-    alert('clicked!');
-  }
   return (<ul>
-    {props.groceries.map(item => <li onClick={listItemClick}>{item}</li>)}
+    {props.groceries.map(item => 
+      <GroceryListItems items={item} />
+    )}
   </ul>)
+}
+
+// GroceryList컴포넌트에 들어갈 list를 class형식으로
+class GroceryListItems extends React.Component{
+  constructor (props){
+    super(props);
+
+    this.state = {
+      done: false,
+      isHovering: false
+    };
+  }
+
+  listClick(){
+    this.setState({
+      done: !this.state.done
+    }); // list를 클릭하면 상태가 바뀐다
+  }
+
+  listHover(){
+    this.setState({
+      isHovering: !this.state.isHovering
+    })
+  }
+
+  render(){
+    const style = {
+      textDecoration : this.state.done ? 'line-through' : 'none',
+      fontWeight : this.state.isHovering ? 'bold' : 'normal'
+    } //textDecoration를 textDeco이런식으로하면 안됨
+    // 클릭할때마다 바뀌는 state에 따라 글자에 선이 생겼다 사라졌다 계속바뀜
+    // const bold = {
+    //   fontWeight : this.state.isHovering ? 'bold' : 'normal'
+    // }
+
+    return (
+      <ul>{console.log(this)} 
+        <li style={style}
+          onMouseEnter={this.listHover.bind(this)}
+          onMouseLeave={this.listHover.bind(this)}
+          onClick={this.listClick.bind(this)}
+        >
+          {this.props.items}
+        </li>
+      </ul>
+    )
+  }
 }
 
 const App = () => (
